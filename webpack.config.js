1c40895bf3f8152e056d7bf9045940abd4b12a68
Kovseg <vladislavkoritskiy@yandex.ru>
2024-06-05 12:03:35 +0300

    added general files - v0.0.1

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
